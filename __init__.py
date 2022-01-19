451449d3514f1bd05bccd85358c022f9e831edc9
Alif Abdullah <aabdullah20@stuy.edu>
2022-01-19 18:02:31 -0500

    stuff

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
+app = Flask(__name__)
 
-@app.route("/") # Q1: What points of reference do you have for meaning of '/'?
+@app.route("/")
 def hello_world():
-    print(__name__) # Q2: Where will this print to? Q3: What will it print?
-    return "No hablo queso!"  # Q3: Will this appear anywhere? How u know?
+    print(__name__)
+    return "No hablo queso!"
 
-app.run()  # Q4: Where have you seen similar construcs in other languages?
-                
+app.run()
+
